a9585c609cb7cc317abea6163f42ab0ee2d3e0ac
Danila Molodtsov <danmol04@gmail.com>
2020-07-08 07:08:09 +0300

    few better

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ class App extends React.Component{
    render() {
       return (
       <div>
-         <span> Кусочек плана цеха с моего ответа по практике
+         <span> Кусочек плана цеха с моего отчета по практике за 2 или 3 курс
          </span>
          <img src={img} style={{width: "400px"}}/>
          
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
